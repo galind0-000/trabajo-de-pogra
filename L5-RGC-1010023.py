318bc7423ba0f56c8f83e83a2a4af15bd05b0737
galind0-000 <144351770+galind0-000@users.noreply.github.com>
2023-09-12 19:20:56 -0600

    Add files via upload

---
+++ L5-RGC-1010023.py
@@ -0,0 +1,14 @@
+print("actividad 1")
+
+num=input("ingrese numero un numero entero")
+
+xnumero = int(num)
+if xnumero > 0:
+    
+  print("el numero es entero")
+else:
+   print("el numero es negativo")
+
+if xnumero ==0:
+    print("el numero es igual a 0")
+
---
+++ L52-RGC-1010023.py
@@ -0,0 +1,29 @@
+print("ejercicio 2")
+
+dia= input("ingrese el numero del dia")
+xdia= int(dia) >0
+
+if xdia == 1:
+      print("lunes")
+
+if xdia == 2:
+    print("martres")
+
+if xdia == 3:
+     print("miercoles")
+
+if xdia == 4:
+     print("jueves")
+
+if xdia == 5:
+     print("viernes")
+
+if xdia == 6:
+     print("sabado")
+
+if xdia == 7:
+     print("domigo")
+
+if xdia > 7:
+     print("error el dia tiene que estar situado de 1 al 7")
+     
